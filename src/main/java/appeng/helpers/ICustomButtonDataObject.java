080472d90efd34957e566bb2ec81f6138284c88e
Kogepan229 <kogepan25629@gmail.com>
2025-11-06 15:42:48 +0900

    Fix dual interface crash

--- src/main/java/appeng/helpers/ICustomButtonDataObject.java
+++ src/main/java/appeng/helpers/ICustomButtonDataObject.java
@@ -5,13 +5,17 @@ import java.util.List;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.nbt.NBTTagCompound;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import io.netty.buffer.ByteBuf;
 
 public interface ICustomButtonDataObject {
 
+    @SideOnly(Side.CLIENT)
     void initCustomButtons(int guiLeft, int guiTop, int xSize, int ySize, int xOffset, int yOffset,
             List<GuiButton> buttonList);
 
+    @SideOnly(Side.CLIENT)
     boolean actionPerformedCustomButtons(final GuiButton btn);
 
     void readData(NBTTagCompound tag);
--- src/main/java/appeng/helpers/ICustomButtonProvider.java
+++ src/main/java/appeng/helpers/ICustomButtonProvider.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import net.minecraft.client.gui.GuiButton;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public interface ICustomButtonProvider {
 
+    @SideOnly(Side.CLIENT)
     void initCustomButtons(int guiLeft, int guiTop, int xSize, int ySize, int xOffset, int yOffset,
             List<GuiButton> buttonList);
 
+    @SideOnly(Side.CLIENT)
     boolean actionPerformedCustomButtons(final GuiButton btn);
 
     void writeCustomButtonData();
